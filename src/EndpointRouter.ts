8718004ec227a899a479aebd4043177dec3e5ea5
Phil Lee <philip@strandedmusician.com>
2019-03-28 17:58:40 -0700

    Setting up linting rules and formatting/adjusting current code to adhere.

--- src/EndpointRouter.ts
+++ src/EndpointRouter.ts
@@ -1,16 +1,16 @@
-import {HTTPEvent, HTTPResponse, HTTPBody, HTTPHeaders, HTTPAction, Validation} from "./Model";
-import { Context, Callback } from "aws-lambda";
-import { HTTPSerializer, JSONRedactingSerializer } from "./serialization";
-import { HTTPEventCondition, endpoint, cors, AllowedOrigin } from "./decorators";
+import { HTTPEvent, HTTPResponse, HTTPBody, HTTPHeaders, HTTPAction, Validation } from './Model';
+import { Context, Callback } from 'aws-lambda';
+import { HTTPSerializer, JSONRedactingSerializer } from './serialization';
+import { HTTPEventCondition, endpoint, cors, AllowedOrigin } from './decorators';
 
 interface EndpointRoute {
-  resource: string;
-  action: string;
-  endpoint: Function;
-  condition?: HTTPEventCondition;
-  priority: number;
-  validations?: Array<Validation>;
-  descriptor: PropertyDescriptor
+    resource: string;
+    action: string;
+    endpoint: Function;
+    condition?: HTTPEventCondition;
+    priority: number;
+    validations?: Array<Validation>;
+    descriptor: PropertyDescriptor;
 }
 
 /**
@@ -19,114 +19,115 @@ interface EndpointRoute {
  */
 export abstract class EndpointRouter {
 
-  private static routes: Array<EndpointRoute> = new Array();
-  private static serializer: HTTPSerializer = new JSONRedactingSerializer();
-  public static validationHandler: any;
+    private static routes: Array<EndpointRoute> = [];
+    private static serializer: HTTPSerializer = new JSONRedactingSerializer();
+    public static validationHandler: any;
 
-  private constructor() { }
-
-  /**
-   * 
-   * @param resource to map this endpoint to.
-   * @param action to map this endpoint to.
-   * @param endpoint function that will take event: HTTPEvent as the first argument 
-   *        and context: LambdaContext as the second argument. It is expected that 
-   *        it will return Promise<HTTPResponse>
-   * @param descriptor the property descriptor from the method, used to bind the
-   *        validation back to the endpoint in the handle() phase
-   * @param condition?, that if provided, will test if this endpoint should even
-   *        be invoked.
-   * @param priority? of this endpoint. A higher value indicates it should be
-   *        executed ahead of other endpoints. Defaults to 0.
-   * 
-   */
-  public static register(resource: string, action: string, endpoint: Function, descriptor: PropertyDescriptor, condition?: HTTPEventCondition, priority?: number): void {
-    EndpointRouter.routes.push({
-      descriptor: descriptor,
-      resource: resource,
-      action: action,
-      endpoint: endpoint,
-      condition: condition,
-      priority: priority || 0
-    });
-  }
-
-  public static attachEndpointValidation(descriptor: PropertyDescriptor, validation: Validation): void {
-    let route = EndpointRouter.routes.find(route => route.descriptor === descriptor);
-    if (route) {
-      if (route.validations) {
-        route.validations.push(validation);
-      } else {
-        route.validations = [validation];
-      }
+    private constructor() {
     }
-  }
 
-  /**
-   * 
-   * @param headers to set on every request.
-   */
-  public static setDefaultHeaders(headers: HTTPHeaders): void {
-    HTTPResponse.setDefaultHeaders(headers);
-  }
+    /**
+     *
+     * @param resource to map this endpoint to.
+     * @param action to map this endpoint to.
+     * @param endpoint function that will take event: HTTPEvent as the first argument
+     *        and context: LambdaContext as the second argument. It is expected that
+     *        it will return Promise<HTTPResponse>
+     * @param descriptor the property descriptor from the method, used to bind the
+     *        validation back to the endpoint in the handle() phase
+     * @param condition?, that if provided, will test if this endpoint should even
+     *        be invoked.
+     * @param priority? of this endpoint. A higher value indicates it should be
+     *        executed ahead of other endpoints. Defaults to 0.
+     *
+     */
+    public static register(resource: string, action: string, endpoint: Function, descriptor: PropertyDescriptor, condition?: HTTPEventCondition, priority?: number): void {
+        EndpointRouter.routes.push({
+            descriptor: descriptor,
+            resource: resource,
+            action: action,
+            endpoint: endpoint,
+            condition: condition,
+            priority: priority || 0
+        });
+    }
 
-  /**
-   * 
-   * @param event to invoke the endpoint with.
-   * @param context of the invocation.
-   * @param callback to execute when completed.
-   */
-  public static handle(event: HTTPEvent, context: Context, callback: Callback<HTTPResponse>): void {
-    const body: HTTPBody | undefined = event.body ? EndpointRouter.serializer.deserializeBody(event.body) : undefined;
-    console.log("EndpointRouter.routes", { resource: event.resource, httpMethod: event.httpMethod });
-    const routeOptions: Array<EndpointRoute> = EndpointRouter.routes
-      .filter((route: EndpointRoute) => route.resource === event.resource)
-      .filter((route: EndpointRoute) => route.action === event.httpMethod)
-      .filter((route: EndpointRoute) => {
-        console.log("check condition", route);
-        if (route.condition) {
-          return route.condition(body, event);
+    public static attachEndpointValidation(descriptor: PropertyDescriptor, validation: Validation): void {
+        const route = EndpointRouter.routes.find(route => route.descriptor === descriptor);
+        if (route) {
+            if (route.validations) {
+                route.validations.push(validation);
+            } else {
+                route.validations = [validation];
+            }
         }
-        return route;
-      })
-      .map((route: EndpointRoute) => {
-        if (route.validations) {
-          route.validations = route.validations.filter((validation) => validation.validatorCondition && validation.validatorCondition(body, event));
-        } else {
-          route.validations = [];
+    }
+
+    /**
+     *
+     * @param headers to set on every request.
+     */
+    public static setDefaultHeaders(headers: HTTPHeaders): void {
+        HTTPResponse.setDefaultHeaders(headers);
+    }
+
+    /**
+     *
+     * @param event to invoke the endpoint with.
+     * @param context of the invocation.
+     * @param callback to execute when completed.
+     */
+    public static handle(event: HTTPEvent, context: Context, callback: Callback<HTTPResponse>): void {
+        const body: HTTPBody | undefined = event.body ? EndpointRouter.serializer.deserializeBody(event.body) : undefined;
+        console.log('EndpointRouter.routes', {resource: event.resource, httpMethod: event.httpMethod});
+        const routeOptions: Array<EndpointRoute> = EndpointRouter.routes
+            .filter((route: EndpointRoute) => route.resource === event.resource)
+            .filter((route: EndpointRoute) => route.action === event.httpMethod)
+            .filter((route: EndpointRoute) => {
+                console.log('check condition', route);
+                if (route.condition) {
+                    return route.condition(body, event);
+                }
+                return route;
+            })
+            .map((route: EndpointRoute) => {
+                if (route.validations) {
+                    route.validations = route.validations.filter((validation) => validation.validatorCondition && validation.validatorCondition(body, event));
+                } else {
+                    route.validations = [];
+                }
+                return route;
+
+            })
+            .sort((first: EndpointRoute, second: EndpointRoute) => second.priority - first.priority);
+
+        if (!routeOptions || routeOptions.length === 0) {
+            return callback(
+                undefined,
+                HTTPResponse
+                    .setBody({message: 'Action is not implemented at this path.'})
+                    .setStatusCode(501)
+                    .addHeader('X-REQUEST-ID', event.requestContext.requestId)
+            );
         }
-        return route;
 
-      })
-      .sort((first: EndpointRoute, second: EndpointRoute) => second.priority - first.priority);
+        let promiseChain = Promise.resolve<HTTPResponse | undefined>(undefined);
 
-    if (!routeOptions || routeOptions.length === 0) {
-      return callback(
-        null,
-        HTTPResponse
-          .setBody({ message: 'Action is not implemented at this path.' })
-          .setStatusCode(501)
-          .addHeader('X-REQUEST-ID', event.requestContext.requestId)
-      );
+        for (const route of routeOptions) {
+            promiseChain = promiseChain.then((response: HTTPResponse | undefined) => {
+                if (response) return response;
+                return route.endpoint(event, context, route.validations);
+            });
+        }
+
+
+        promiseChain
+            .then((response: HTTPResponse | undefined) => callback(undefined, response))
+            .catch((error: any) => callback(error));
+
     }
 
-    let promiseChain = Promise.resolve<HTTPResponse | undefined>(undefined);
-
-    for (let route of routeOptions) {
-      promiseChain = promiseChain.then((response: HTTPResponse | undefined) => {
-        if (response) return response;
-        return route.endpoint(event, context, route.validations)
-      })
+    static attachValidationHandler(validationHandler: any) {
+        EndpointRouter.validationHandler = validationHandler;
     }
-
-
-    promiseChain
-      .then((response: HTTPResponse | undefined) => callback(null, response))
-      .catch((error: any) => callback(error))
-
-  }
-
-  static attachValidationHandler(validationHandler: any) {
-    EndpointRouter.validationHandler = validationHandler;
-  }
 }
--- src/decorators.ts
+++ src/decorators.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
-import {HTTPHeaders, HTTPResponse, HTTPEvent, HTTPAction, HTTPBody, Validation} from "./Model";
+import { HTTPHeaders, HTTPResponse, HTTPEvent, HTTPAction, HTTPBody, Validation } from './Model';
 import { EndpointRouter } from './EndpointRouter';
 import { JSONRedactingSerializer } from './Serialization';
-import {version} from "punycode";
+import { version } from 'punycode';
 
 export const METADATA_KEY: string = '__MU-TS';
 export const REDACTED_KEY: string = 'redacted';
@@ -10,59 +10,59 @@ export const REDACTED_KEY: string = 'redacted';
 /**
  * Used to ensure that values in a model are removed from the response
  * when it is being serialized.
- * 
+ *
  * @param target object where the attribute is being redacted.
  * @param propertyKey of the attribute being redacted.
  * @param descriptor of the attribute being redacted.
  */
 export function redacted(target: any, propertyToRedact: string) {
-  const metadata = Reflect.getMetadata(METADATA_KEY, target) || {};
-  const redactedKeys = metadata[REDACTED_KEY] || [];
+    const metadata = Reflect.getMetadata(METADATA_KEY, target) || {};
+    const redactedKeys = metadata[REDACTED_KEY] || [];
 
-  redactedKeys.push(propertyToRedact);
+    redactedKeys.push(propertyToRedact);
 
-  metadata[REDACTED_KEY] = redactedKeys;
+    metadata[REDACTED_KEY] = redactedKeys;
 
-  Reflect.defineMetadata(METADATA_KEY, metadata, target);
-};
+    Reflect.defineMetadata(METADATA_KEY, metadata, target);
+}
 
 export interface AllowedOrigin {
-  (event: HTTPEvent, response: HTTPResponse): string;
+    (event: HTTPEvent, response: HTTPResponse): string;
 }
 
 /**
  * Needs to be placed after the @endpoints decorator.
- * 
+ *
  * @param Defines the COR's configuration for a specific endpoint.
  */
 export function cors(allowedOrigin: string | AllowedOrigin, allowedActions?: Array<HTTPAction | string>, allowedHeaders?: HTTPHeaders, allowCredentials: boolean = true) {
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 
-    const targetMethod = descriptor.value;
+        const targetMethod = descriptor.value;
 
-    descriptor.value = function () {
+        descriptor.value = function () {
 
-      const event: HTTPEvent = arguments[0];
+            const event: HTTPEvent = arguments[0];
 
-      return targetMethod.apply(this, arguments)
-        .then((response: HTTPResponse) => {
+            return targetMethod.apply(this, arguments)
+                .then((response: HTTPResponse) => {
 
-          const origin: string = typeof allowedOrigin === 'string' ? allowedOrigin : allowedOrigin(event, response);
+                    const origin: string = typeof allowedOrigin === 'string' ? allowedOrigin : allowedOrigin(event, response);
 
-          response.addHeaders({
-            'Access-Control-Allow-Origin': origin,
-            'Access-Control-Allow-Credentials': allowCredentials ? 'true' : 'false'
-          })
+                    response.addHeaders({
+                        'Access-Control-Allow-Origin': origin,
+                        'Access-Control-Allow-Credentials': allowCredentials ? 'true' : 'false'
+                    });
 
-          if (allowedActions) response.addHeader('Access-Control-Allow-Methods', allowedActions.join(', '));
-          if (allowedHeaders) response.addHeader('Access-Control-Allow-Headers', Object.keys(allowedHeaders).join(', '));
+                    if (allowedActions) response.addHeader('Access-Control-Allow-Methods', allowedActions.join(', '));
+                    if (allowedHeaders) response.addHeader('Access-Control-Allow-Headers', Object.keys(allowedHeaders).join(', '));
 
-          return response;
-        });
-    }
+                    return response;
+                });
+        };
 
-    return descriptor;
-  };
+        return descriptor;
+    };
 }
 
 /**
@@ -70,7 +70,7 @@ export function cors(allowedOrigin: string | AllowedOrigin, allowedActions?: Arr
  * should be executed or not.
  */
 export interface HTTPEventCondition {
-  (body: HTTPBody | undefined, event: HTTPEvent): boolean;
+    (body: HTTPBody | undefined, event: HTTPEvent): boolean;
 }
 
 /**
@@ -78,46 +78,46 @@ export interface HTTPEventCondition {
  * @param route for this function.
  */
 export function endpoint(path: string, action: HTTPAction | string, condition?: HTTPEventCondition, priority?: number) {
-  return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
 
-    const targetMethod = descriptor.value;
+        const targetMethod = descriptor.value;
 
-    descriptor.value = function () {
+        descriptor.value = function () {
 
-      const event: HTTPEvent = arguments[0];
-      const validations: Array<Validation> = arguments[2];
+            const event: HTTPEvent = arguments[0];
+            const validations: Array<Validation> = arguments[2];
 
-      if (validations) {
-          let validationErrors = new Set<string>();
-          validations.forEach(validation => {
-              validationErrors.add(EndpointRouter.validationHandler.validate(event.body, validation.schema));
-          });
+            if (validations) {
+                const validationErrors = new Set<string>();
+                validations.forEach(validation => {
+                    validationErrors.add(EndpointRouter.validationHandler.validate(event.body, validation.schema));
+                });
 
-          if (validationErrors.size) {
-              return HTTPResponse
-                  .setBody({ message: validationErrors })
-                  .setStatusCode(400)
-                  .addHeader('X-REQUEST-ID', event.requestContext.requestId);
-          }
-      }
+                if (validationErrors.size) {
+                    return HTTPResponse
+                        .setBody({message: validationErrors})
+                        .setStatusCode(400)
+                        .addHeader('X-REQUEST-ID', event.requestContext.requestId);
+                }
+            }
 
-      return targetMethod.apply(this, arguments)
-        .then((response: HTTPResponse) => {
-          response.addHeader('X-REQUEST-ID', event.requestContext.requestId);
-          return response;
-        })
-        .catch((error: any) => {
-          return HTTPResponse
-            .setBody({ message: error.message })
-            .setStatusCode(501)
-            .addHeader('X-REQUEST-ID', event.requestContext.requestId);
-        });
-    }
+            return targetMethod.apply(this, arguments)
+                .then((response: HTTPResponse) => {
+                    response.addHeader('X-REQUEST-ID', event.requestContext.requestId);
+                    return response;
+                })
+                .catch((error: any) => {
+                    return HTTPResponse
+                        .setBody({message: error.message})
+                        .setStatusCode(501)
+                        .addHeader('X-REQUEST-ID', event.requestContext.requestId);
+                });
+        };
 
-    const routeAction = typeof action === 'string' ? action.toUpperCase() : action;
+        const routeAction = typeof action === 'string' ? action.toUpperCase() : action;
 
-    EndpointRouter.register(path, routeAction.toUpperCase(), descriptor.value, descriptor, condition, priority);
+        EndpointRouter.register(path, routeAction.toUpperCase(), descriptor.value, descriptor, condition, priority);
 
-    return descriptor;
-  };
+        return descriptor;
+    };
 }
